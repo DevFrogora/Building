454a59abec83d721157f44af2bbd252685d06686
DevFrogora <rr4428310@gmail.com>
2022-06-18 06:38:04 +0100

    Working on BagInventory

---
+++ Assets/InsideBag/Slot2/DraggableSlot2AssultUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableSlot2AssultUI : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform canvasTransform;
+    Transform parentToReturnTo = null;
+    CanvasGroup canvasGroup;
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        parentToReturnTo = transform.parent;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        transform.SetParent(canvasTransform);
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentToReturnTo);
+        canvasGroup.blocksRaycasts = true;
+    }
+
+
+}
---
+++ Assets/InsideBag/Slot2/DropzoneSlot2UI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DropzoneSlot2UI : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped to " + gameObject.name);
+    }
+}
